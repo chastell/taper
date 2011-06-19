609b47e8a547c54aeec19b1a9e270b09330f6591
Piotr Szotkowski <chastell@chastell.net>
2011-06-19 23:11:51 +0200

    define the Provider interface

--- lib/taper/executable.rb
+++ lib/taper/executable.rb
@@ -4,6 +4,11 @@ module Taper class Executable
     Trollop.options
     Trollop.die 'no file given'       if     args.empty?
     Trollop.die 'file does not exist' unless File.exists? args.first
+    @movie = args.first
+  end
+
+  def run args
+    args[:output].puts args[:provider].subtitles_for @movie
   end
 
 end end
--- spec/taper/executable_spec.rb
+++ spec/taper/executable_spec.rb
@@ -1,4 +1,10 @@
+# encoding: UTF-8
+
 module Taper describe Executable do
+
+  before do
+    @movie = 'spec/fixtures/movie'
+  end
 
   describe '.new' do
 
@@ -26,8 +32,20 @@ module Taper describe Executable do
     end
 
     it 'does not whine if the file exists' do
-      Executable.new ['spec/fixtures/movie']
+      Executable.new [@movie]
       stderr.should be_empty
+    end
+
+  end
+
+  describe '#run' do
+
+    it 'passes the file to the provider and prints the result on output' do
+      provider = mock
+      provider.should_receive(:subtitles_for).and_return '…subtitles…'
+      Executable.new([@movie]).run output: output = StringIO.new, provider: provider
+      output.rewind
+      output.read.should == "…subtitles…\n"
     end
 
   end
