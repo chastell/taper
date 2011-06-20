f5deeedc328f84039710dd0703a8ce6fade58cc6
Piotr Szotkowski <chastell@chastell.net>
2011-06-21 00:13:37 +0200

    Providers::NapiProjekt: magic, got it.

--- lib/taper/providers/napi_projekt.rb
+++ lib/taper/providers/napi_projekt.rb
@@ -1,2 +1,14 @@
 module Taper module Providers::NapiProjekt
+
+  extend self
+
+  # courtesy of sites.google.com/site/napipy
+  # FIXME: privatise
+  def magic hex
+    [[14,0,2], [3,13,2], [6,16,5], [8,11,4], [2,5,3]].map do |a, b, c|
+      from = hex[a].to_i(16) + b
+      (hex[from, 2].to_i(16) * c).to_s(16)[-1]
+    end.join
+  end
+
 end end
--- spec/taper/providers/napi_projekt_spec.rb
+++ spec/taper/providers/napi_projekt_spec.rb
@@ -1,2 +1,21 @@
 module Taper describe Providers::NapiProjekt do
+
+  describe '.magic' do
+
+    it 'returns the right magic String for inclusion in NapiProjekt requests' do
+      {
+        '193b79bac996e3d5c25bcf87a056527d' => 'a098b',
+        '5f34093f90cf47c85f3bea70c500d09e' => 'e6680',
+        '7ec948ba757d064bfb8f5bc5f30ccf8d' => '0acc8',
+        '8b6307875f2b65df8c944dcc12c74610' => 'a8a02',
+        '8ddc258cd1451017a54dafa8b977e89b' => 'aed47',
+        '974fbce02821e99135ed1f0c9a990b91' => '46546',
+        'c204f839efcd562ba8ca7311bc3eb0d4' => '883c9',
+      }.each do |hex, magic|
+        Providers::NapiProjekt.magic(hex).should == magic
+      end
+    end
+
+  end
+
 end end
