fd825101beeddff2584dd2aebf047bd7ea64601a
Piotr Szotkowski <chastell@chastell.net>
2011-06-22 20:32:46 +0200

    Providers::NapiProjekt: putting some magic in the URLs

--- lib/taper/providers/napi_projekt.rb
+++ lib/taper/providers/napi_projekt.rb
@@ -4,6 +4,13 @@ module Taper module Providers::NapiProjekt
 
   # courtesy of sites.google.com/site/napipy
   # FIXME: privatise
+  def url_for hex
+    "http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=#{hex}&t=#{magic hex}&v=other&kolejka=false&nick=&pass=&napios=posix"
+  end
+
+  private
+
+  # courtesy of sites.google.com/site/napipy
   def magic hex
     [[14,0,2], [3,13,2], [6,16,5], [8,11,4], [2,5,3]].map do |a, b, c|
       from = hex[a].to_i(16) + b
--- spec/taper/providers/napi_projekt_spec.rb
+++ spec/taper/providers/napi_projekt_spec.rb
@@ -1,18 +1,18 @@
 module Taper describe Providers::NapiProjekt do
 
-  describe '.magic' do
+  describe '.url_for' do
 
-    it 'returns the right magic String for inclusion in NapiProjekt requests' do
+    it 'returns the right magic URL for inclusion in NapiProjekt requests' do
       {
-        '193b79bac996e3d5c25bcf87a056527d' => 'a098b',
-        '5f34093f90cf47c85f3bea70c500d09e' => 'e6680',
-        '7ec948ba757d064bfb8f5bc5f30ccf8d' => '0acc8',
-        '8b6307875f2b65df8c944dcc12c74610' => 'a8a02',
-        '8ddc258cd1451017a54dafa8b977e89b' => 'aed47',
-        '974fbce02821e99135ed1f0c9a990b91' => '46546',
-        'c204f839efcd562ba8ca7311bc3eb0d4' => '883c9',
-      }.each do |hex, magic|
-        Providers::NapiProjekt.magic(hex).should == magic
+        '193b79bac996e3d5c25bcf87a056527d' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=193b79bac996e3d5c25bcf87a056527d&t=a098b&v=other&kolejka=false&nick=&pass=&napios=posix',
+        '5f34093f90cf47c85f3bea70c500d09e' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=5f34093f90cf47c85f3bea70c500d09e&t=e6680&v=other&kolejka=false&nick=&pass=&napios=posix',
+        '7ec948ba757d064bfb8f5bc5f30ccf8d' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=7ec948ba757d064bfb8f5bc5f30ccf8d&t=0acc8&v=other&kolejka=false&nick=&pass=&napios=posix',
+        '8b6307875f2b65df8c944dcc12c74610' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=8b6307875f2b65df8c944dcc12c74610&t=a8a02&v=other&kolejka=false&nick=&pass=&napios=posix',
+        '8ddc258cd1451017a54dafa8b977e89b' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=8ddc258cd1451017a54dafa8b977e89b&t=aed47&v=other&kolejka=false&nick=&pass=&napios=posix',
+        '974fbce02821e99135ed1f0c9a990b91' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=974fbce02821e99135ed1f0c9a990b91&t=46546&v=other&kolejka=false&nick=&pass=&napios=posix',
+        'c204f839efcd562ba8ca7311bc3eb0d4' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=c204f839efcd562ba8ca7311bc3eb0d4&t=883c9&v=other&kolejka=false&nick=&pass=&napios=posix',
+      }.each do |hex, url|
+        Providers::NapiProjekt.url_for(hex).should == url
       end
     end
 
