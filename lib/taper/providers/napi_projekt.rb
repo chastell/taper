7b6f06c4001e3286776ecd61fef7b41a47ad7686
Piotr Szotkowski <chastell@chastell.net>
2011-06-23 23:51:44 +0200

    Providers::NapiProjekt: pretend to implement the interface

--- lib/taper/providers/napi_projekt.rb
+++ lib/taper/providers/napi_projekt.rb
@@ -2,10 +2,8 @@ module Taper module Providers::NapiProjekt
 
   extend self
 
-  # courtesy of sites.google.com/site/napipy
-  # FIXME: privatise
-  def url_for hash
-    "http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=#{hash}&t=#{magic_for hash}&v=other"
+  def subtitles_for hash
+    url_for hash
   end
 
   private
@@ -16,6 +14,11 @@ module Taper module Providers::NapiProjekt
       from = hash[a].to_i(16) + b
       (hash[from, 2].to_i(16) * c).to_s(16)[-1]
     end.join
+  end
+
+  # courtesy of sites.google.com/site/napipy
+  def url_for hash
+    "http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=#{hash}&t=#{magic_for hash}&v=other"
   end
 
 end end
--- spec/taper/providers/napi_projekt_spec.rb
+++ spec/taper/providers/napi_projekt_spec.rb
@@ -1,8 +1,8 @@
 module Taper describe Providers::NapiProjekt do
 
-  describe '.url_for' do
+  describe '.subtitles_for' do
 
-    it 'returns the right magic URL for inclusion in NapiProjekt requests' do
+    it '(temporarily) returns the right magic URL for inclusion in NapiProjekt requests' do
       {
         '193b79bac996e3d5c25bcf87a056527d' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=193b79bac996e3d5c25bcf87a056527d&t=a098b&v=other',
         '5f34093f90cf47c85f3bea70c500d09e' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=5f34093f90cf47c85f3bea70c500d09e&t=e6680&v=other',
@@ -12,7 +12,7 @@ module Taper describe Providers::NapiProjekt do
         '974fbce02821e99135ed1f0c9a990b91' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=974fbce02821e99135ed1f0c9a990b91&t=46546&v=other',
         'c204f839efcd562ba8ca7311bc3eb0d4' => 'http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=c204f839efcd562ba8ca7311bc3eb0d4&t=883c9&v=other',
       }.each do |hash, url|
-        Providers::NapiProjekt.url_for(hash).should == url
+        Providers::NapiProjekt.subtitles_for(hash).should == url
       end
     end
 
